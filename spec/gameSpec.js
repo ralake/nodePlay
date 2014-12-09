4422bf19666d147569832e11d7f1767ae52cd22c
Richard Lake <ralake1985@gmail.com>
2014-12-09 14:31:04 +0000

    Finish winning and losing functionality

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,12 +6,44 @@ describe('game', function() {
   var computer = jasmine.createSpy('computer');
   var game = new Game(player, computer);
 
-  it('should initialize with one player', function() {
-    expect(game.player).toEqual(player);
-  });
+  describe('when the game starts...', function() {
 
-  it('should initialize with one computer player', function() {
-    expect(game.computer).toEqual(computer);
-  });
+	  it('should initialize with one player', function() {
+	    expect(game.player).toEqual(player);
+	  });
+
+	  it('should initialize with one computer player', function() {
+	    expect(game.computer).toEqual(computer);
+	  });
+
+	});
+
+	describe('should know when to draw a match', function() {
+
+		it('two rocks equal a draw', function() {
+			expect(game.winner('rock', 'rock')).toEqual('draw')
+		});
+
+		it('two rocks equal a draw', function() {
+			expect(game.winner('paper', 'paper')).toEqual('draw')
+		});
+
+		it('two rocks equal a draw', function() {
+			expect(game.winner('scissors', 'scissors')).toEqual('draw')
+		});
+
+	});
+
+	describe('it should know when a player wins', function() {
+
+		it('between rock and scissors', function() {
+			expect(game.winner('rock', 'scissors')).toEqual('player wins!')
+		});
+
+		it('between rock and paper', function() {
+			expect(game.winner('rock', 'paper')).toEqual('computer wins!')
+		});
+
+	});
 
 });
--- src/game.js
+++ src/game.js
@@ -3,4 +3,17 @@ function Game(player, computer) {
   this.computer = computer;
 }
 
+Game.prototype.winner = function(playerChoice, computerChoice) {
+  var winningCombos = { rock: 'scissors', scissors: 'paper', paper: 'rock'}
+  if (playerChoice === computerChoice) {
+  	return 'draw';
+  }
+  else if (winningCombos[playerChoice] === computerChoice) {
+  	return 'player wins!'
+  }
+  else {
+  	return 'computer wins!'
+  }
+};
+
 module.exports = Game; 
